d26f331a5b35e10f2adf13849ba9fd891b905906
Anastasija <anastasijamarsenic@gmail.com>
2024-01-23 16:56:04 +0100

    Initial project setup

---
+++ med_equipment_location_simulator/src/main/java/com/isa/med_equipment_location_simulator/MedEquipmentLocationSimulatorApplication.java
@@ -0,0 +1,14 @@
+package com.isa.med_equipment_location_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedEquipmentLocationSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedEquipmentLocationSimulatorApplication.class, args);
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ med_equipment_location_simulator/src/test/java/com/isa/med_equipment_location_simulator/MedEquipmentLocationSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.med_equipment_location_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedEquipmentLocationSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
